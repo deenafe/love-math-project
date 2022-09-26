88f67fe623ec4253293e5e807d5848d0bcdaae8e
deenafe <tymbalink@hotmail.com>
2022-09-26 15:17:12 +0000

    Add js function for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,7 @@ function runGame(gameType) {
 
     let num1 = Math.floor(Math.random() * 25) + 1
     let num2 = Math.floor(Math.random() * 25) + 1
+    let num3 = num1 * num2
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2)
@@ -37,6 +38,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2)
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2)
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num3, num2)
     } else {
         alert(`Unknown game type: ${gameType}`)
         throw `Unknown game type: ${gameType}. Aborting!`
@@ -69,6 +72,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"]
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"]
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"]
     } else {
         alert(`Unimplemented Operator ${operator}`)
         throw `Unimplemented Operator ${operator}. Aborting!`
@@ -109,6 +114,14 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1
     document.getElementById("operand2").textContent = operand2
     document.getElementById("operator").textContent = "x"
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1
+    document.getElementById("operand2").textContent = operand2
+    document.getElementById("operator").textContent = "/"
+    
 }
 
 
